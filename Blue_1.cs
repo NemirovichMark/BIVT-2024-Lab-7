28783d0a0f1cab32380d925ba658ca8a7bdf40dd
Ggh13 <62508921+Ggh13@users.noreply.github.com>
2025-02-16 10:29:34 +0000

    Add files via upload

---
+++ Blue_1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Blue_1
+    {
+        public struct Response
+        {
+
+            private string name;
+            private string surname;
+            private int votes;
+
+            public string Name
+            {
+                get
+                {
+                    if (name == null)
+                        return "";
+                    return name;
+                }
+            }
+
+            public string Surname
+            {
+                get
+                {
+                    if (surname == null)
+                        return "";
+                    return surname;
+                }
+            }
+
+            public int Votes
+            {
+                get
+                {
+                    if (votes == null)
+                        return 0;
+                    return votes;
+                }
+            }
+
+            public Response(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.votes = 0;
+            }
+
+            public int CountVotes(Response[] responses)
+            {
+
+                int count = 0;
+                foreach (var response in responses)
+                {
+                    if (response.Name == this.Name && response.Surname == this.Surname)
+                    {
+                        count++;
+                    }
+                }
+
+                this.votes = count;
+                return count;
+            }
+        }
+    }
+}
