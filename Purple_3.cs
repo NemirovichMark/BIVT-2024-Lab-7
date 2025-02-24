9e093c7abef6c3014cd055983352fe924717f2e9
Mover-R <denis.tugow2014@gmail.com>
2025-02-24 23:24:21 +0300

    Done

--- Purple_3.cs
+++ Purple_3.cs
@@ -46,19 +46,159 @@ namespace Lab_6
                 if (_marks == null) return;
                 _marks[_indexJudge++] = result;
             }
+            public int Score
+            {
+                get
+                {
+                    if (_places is null) return 0;
+                    int s = 0;
+                    for (int i = 0; i < _places.Length; i++) s += _places[i];
+                    return s;
+                }
+            }
+
+            private double TotalMark
+            {
+                get
+                {
+                    if (_marks is null) return 0;
+
+                    double s = 0;
+                    for (int i = 0; i < _marks.Length; i++) s += _marks[i];
+
+                    return s;
+                }
+            }
+
+            private int TopPlace
+            {
+                get
+                {
+                    if (_places == null) return 0;
+                    return _places.Min();
+                }
+            }
             public static void SetPlaces(Participant[] participants)
             {
                 if (participants == null) return;
-                for (int i = 0; i<7; i++)
-                {
-                    //Array.Sort(array, (x, y) => x.Marks)
-                }
 
+                for (int judge = 0; judge < 7; judge++)
+                {
+                    for (int i = 1, j = 2; i < participants.Length;)
+                    {
+                        if (i == 0)
+                        {
+                            i = j++;
+                        }
+                        else if (participants[i]._marks == null)
+                        {
+                            i = j++;
+                        }
+                        else if (participants[i - 1]._marks == null)
+                        {
+                            Participant tmp = participants[i];
+                            participants[i] = participants[i - 1];
+                            participants[i - 1] = tmp;
+                            i--;
+                        }
+                        else if (participants[i].Marks[judge] < participants[i - 1].Marks[judge])
+                        {
+                            i = j++;
+                        }
+                        else
+                        {
+                            Participant tmp = participants[i];
+                            participants[i] = participants[i - 1];
+                            participants[i - 1] = tmp;
+                            i--;
+                        }
+                    }
+                    for (int place = 0; place < participants.Length; place++)
+                    {
+                        if (participants[place]._places != null) participants[place]._places[judge] = place + 1;
+                    }
+
+                }
             }
+
             public static void Sort(Participant[] array)
             {
-                if (array == null) return;
-                Array.Sort(array);
+                if (array == null || array.Length < 2) return;
+
+                for (int i = 1, j = 2; i < array.Length;)
+                {
+                    if (i == 0)
+                    {
+                        i = j;
+                        j++;
+                        continue;
+                    }
+                    int score1 = array[i].Score, score2 = array[i - 1].Score;
+                    if (score1 > score2)
+                    {
+                        i = j;
+                        j++;
+                    }
+                    else if (score1 < score2)
+                    {
+                        Participant tmp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = tmp;
+                        i--;
+                    }
+                    else
+                    {
+                        if (score1 == score2 && score1 == 0)
+                        {
+                            i = j;
+                            j++;
+                            continue;
+                        }
+                        int isHigher = 0;
+                        int[] places1 = array[i].Places, places2 = array[i - 1].Places;
+                        for (int k = 0; k < 7; k++)
+                        {
+                            if (places1[k] < places2[k])
+                            {
+                                isHigher = 1;
+                                break;
+                            }
+                            else if (places1[k] < places2[k])
+                            {
+                                isHigher = -1;
+                                break;
+                            }
+                        }
+                        if (isHigher == 1)
+                        {
+                            Participant tmp = array[i];
+                            array[i] = array[i - 1];
+                            array[i - 1] = tmp;
+                            i--;
+                            continue;
+                        }
+                        else if (isHigher == -1)
+                        {
+                            i = j;
+                            j++;
+                            continue;
+                        }
+
+                        double totalMark1 = array[i].TotalMark, totalMark2 = array[i - 1].TotalMark;
+                        if (totalMark1 < totalMark2)
+                        {
+                            i = j;
+                            j++;
+                        }
+                        else
+                        {
+                            Participant tmp = array[i];
+                            array[i] = array[i - 1];
+                            array[i - 1] = tmp;
+                            i--;
+                        }
+                    }
+                }
             }
         }
     }
