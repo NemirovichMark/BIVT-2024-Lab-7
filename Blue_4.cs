f6b9b6647a31a3fe6287e4f05e66fe36171e0caf
Marina Rovnyagina <m2405735@edu.misis.ru>
2025-02-24 17:25:41 +0300

    Add files via upload

---
+++ Blue_4.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Blue_4
+    {
+        public struct Team
+        {
+            //поля
+            private string _name;
+            private int[] _scores;
+
+            //свойства
+            public string Name => _name;
+            public int[] Scores
+            {
+                get
+                {
+                    if (_scores == null) { return null; }
+                    int[] copy = new int[_scores.Length];
+                    Array.Copy(Scores, copy, _scores.Length);
+                    return copy;
+                }
+            }
+            public int TotalScore
+            {
+                get
+                {   
+                    int total = 0;
+                    for (int i = 0; i < _scores.Length; i++)
+                    {
+                        total += _scores[i];
+                    }
+                    return total;
+                }               
+            }
+
+            //конструктор
+            public Team(string name)
+            {
+                _name = name;
+                _scores = new int[0];
+            }
+
+            //метод
+            public void PlayMatch(int result)
+            {
+                Array.Resize(ref _scores, _scores.Length + 1);
+                _scores[_scores.Length - 1] = result;
+            }
+            
+            public void Print()
+            {
+                Console.WriteLine($"Команда: {Name}, Очки: {string.Join(", ", Scores)}, Всего очков: {TotalScore}");
+            }
+
+        }
+
+        public struct Group
+        {
+            private string _name;        //название группы
+            private Team[] _teams;       //массив команд (где каждый элемент является объектом типа Team!)
+
+            public string Name => _name;
+            public Team[] Teams 
+            {
+                get
+                {
+                    if (_teams == null) { return null; }
+                    Team[] copy = new Team[_teams.Length ];
+                    Array.Copy(Teams, copy, _teams.Length);
+                    return copy;
+                }
+            }
+            
+            public Group(string name)
+            {
+                _name = name;
+                _teams = new Team[0];
+            }
+
+            public void Add(Team team)
+            {
+                if (_teams.Length < 12) 
+                {
+                    Array.Resize(ref _teams, _teams.Length + 1);
+                    _teams[_teams.Length - 1] = team;
+                }
+                else
+                {
+                    Console.WriteLine("12 команд набрано.");
+                }
+            }
+            public void Add(params Team[] teams)
+            {
+                foreach (var team in teams)
+                {
+                    if (_teams.Length < 12)
+                    {
+                        Add(team);
+                    }
+                    else
+                    {
+                        Console.WriteLine("12 команд набрано.");
+                        break;
+                    }
+                }
+            }
+            public void Sort()
+            {
+                for (int i = 0; i < _teams.Length - 1; i++)
+                {
+                    for (int j = 0; j < _teams.Length - 1 - i; j++)
+                    {
+                        if (_teams[j].TotalScore < _teams[j + 1].TotalScore)
+                        {                            
+                            Team temp = _teams[j];
+                            _teams[j] = _teams[j + 1];
+                            _teams[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            public static Group Merge(Group group1, Group group2, int size)
+            {
+                Group finalists = new Group("Финалисты");
+
+                int index1 = 0;
+                int index2 = 0;
+
+                // Объединяем команды до достижения максимального размера
+                while (index1 < group1._teams.Length && index2 < group2._teams.Length && finalists._teams.Length < size)
+                {
+                    if (group1._teams[index1].TotalScore >= group2._teams[index2].TotalScore)
+                    {
+                        finalists.Add(group1._teams[index1]);
+                        index1++;
+                    }
+                    else
+                    {
+                        finalists.Add(group2._teams[index2]);
+                        index2++;
+                    }
+                }
+
+                // Если остались команды в первой группе
+                while (index1 < group1._teams.Length && finalists._teams.Length < size)
+                {
+                    finalists.Add(group1._teams[index1]);
+                    index1++;
+                }
+
+                // Если остались команды во второй группе
+                while (index2 < group2._teams.Length && finalists._teams.Length < size)
+                {
+                    finalists.Add(group2._teams[index2]);
+                    index2++;
+                }
+
+                return finalists;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Группа: {_name}");
+                Console.WriteLine("Команды:");
+                foreach (var team in _teams)
+                {
+                    team.Print();
+                }
+            }
+
+        }
+    }
+  
+    
+}
---
+++ Blue_5.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Lab_6.Blue_4;
+using static Lab_6.Blue_5;
+
+namespace Lab_6
+{
+    public class Blue_5
+    {
+        public struct Sportsman
+        {
+            private string _name;
+            private string _surname;
+            private int _place;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Place => _place;
+
+            public Sportsman(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _place = 0;
+            }
+            public void SetPlace(int place)
+            {
+                if (_place == 0)
+                {
+                    _place = place;
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Место: {_place}");
+            }
+        }
+        public struct Team
+        {
+            private string _name;
+            private Sportsman[] _sportsmen;
+
+            public string Name => _name;
+            public Sportsman[] Sportsmen => _sportsmen;
+
+            public Team(string name)
+            {
+                _name = name;
+                _sportsmen = new Sportsman[6];
+            }
+
+            public int SummaryScore
+            {
+                get
+                {
+                    int score = 0;
+                    foreach (var sportsman in _sportsmen)
+                    {
+                        switch (sportsman.Place)
+                        {
+                            case 1:
+                                score += 5;
+                                break;
+                            case 2:
+                                score += 4;
+                                break;
+                            case 3:
+                                score += 3;
+                                break;
+                            case 4:
+                                score += 2;
+                                break;
+                            case 5:
+                                score += 1;
+                                break;
+                            default:
+                                score += 0;
+                                break;
+                        }
+                    }
+                    return score;
+                }
+            }
+            public int TopPlace
+            {
+                get
+                {
+                    int topPlace = int.MaxValue;
+                    foreach (var sportsman in _sportsmen)
+                    {
+                        if (sportsman.Place < topPlace)
+                        {
+                            topPlace = sportsman.Place;
+                        }
+                    }
+                    return topPlace;
+                }
+            }
+            public void Add(Sportsman sportsman)
+            {
+                Array.Resize(ref _sportsmen, _sportsmen.Length + 1);
+                _sportsmen[_sportsmen.Length - 1] = sportsman;
+            }
+
+            public void Add(Sportsman[] sportsmen)
+            {
+                foreach (var sportsman in sportsmen)
+                {
+                    Add(sportsman);
+                }
+            }
+           
+            public static void Sort(Team[] teams)
+            {
+                if (teams == null || teams.Length == 0) { return;}
+                for (int i = 0; i < teams.Length - 1; i++)
+                {
+                    for (int j = 0; j < teams.Length - 1 - i; j++)
+                    {
+                        if (ShouldSwap(teams[j], teams[j + 1]))
+                        {                            
+                            Team temp = teams[j];
+                            teams[j] = teams[j + 1];
+                            teams[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            private static bool ShouldSwap(Team team1, Team team2)
+            {
+                if (team1.SummaryScore != team2.SummaryScore)
+                {
+                    return team1.SummaryScore < team2.SummaryScore;
+                }
+                return team1.TopPlace > team2.TopPlace;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Команда: {Name}");
+                foreach (var sportsman in _sportsmen)
+                {
+                    sportsman.Print();
+                }
+                Console.WriteLine($"Суммарный балл: {SummaryScore}, Наивысшее место: {TopPlace}");
+            }
+        }
+    }
+}
+
