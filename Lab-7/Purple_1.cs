b3f5b8054cae97e1384fe2f2366c815341a56202
AnikinSimon <s.anikin1309@gmail.com>
2025-03-07 15:14:59 +0300

    Add Task files

---
+++ Lab-7/Purple_1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab_7
+{
+    public class Purple_1
+    {
+        public class Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _coefs;
+            private int[,] _marks;
+            private int _jumpIndex;
+
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+            }
+
+            public string Surname
+            {
+                get
+                {
+                    return _surname;
+                }
+            }
+
+            public double[] Coefs
+            {
+                get
+                {
+                    if (_coefs is null) return null;
+                    double[] coefsCopy = new double[_coefs.Length];
+                    Array.Copy(_coefs, coefsCopy, _coefs.Length);
+                    return coefsCopy;
+                }
+            }
+
+            public int[,] Marks
+            {
+                get
+                {
+                    if (_marks is null) return null;
+                    int[,] marksCopy = new int[_marks.GetLength(0), _marks.GetLength(1)];
+                    for (int i = 0; i < _marks.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < _marks.GetLength(1); j++)
+                        {
+                            marksCopy[i, j] = _marks[i, j];
+                        }
+                    }
+                    return marksCopy;
+                }
+            }
+
+            public double TotalScore
+            {
+                get
+                {
+                    if (_marks is null || _coefs is null)
+                    {
+                        Console.WriteLine("error");
+                        return 0;
+                    }
+
+                    int coefsLen = _marks.GetLength(0), marksLen = _marks.GetLength(1);
+                    double score = 0;
+                    for (int i = 0; i < coefsLen; i++)
+                    {
+                        int markSum = 0, maxMarkIdx = 0, minMarkIdx = 0;
+                        for (int j = 0; j < marksLen; j++)
+                        {
+                            markSum += _marks[i, j];
+                            if (_marks[i, j] >= _marks[i, maxMarkIdx])
+                            {
+                                maxMarkIdx = j;
+                            }
+                            if (_marks[i, j] <= _marks[i, minMarkIdx])
+                            {
+                                minMarkIdx = j;
+
+                            }
+
+                        }
+                        markSum -= _marks[i, maxMarkIdx];
+                        markSum -= _marks[i, minMarkIdx];
+
+                        score += markSum * _coefs[i];
+                    }
+                    return score;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _coefs = new double[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    _coefs[i] = 2.5;
+                }
+                _marks = new int[4, 7];
+                _jumpIndex = 0;
+            }
+
+            public void SetCriterias(double[] coefs)
+            {
+                if (_coefs is null || coefs is null || coefs.Length != 4)
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (coefs[i] < 2.5 || coefs[i] > 3.5)
+                    {
+                        return;
+                    }
+                }
+                Array.Copy(coefs, _coefs, 4);
+            }
+
+            public void Jump(int[] marks)
+            {
+                if (marks is null || marks.Length != 7)
+                {
+                    return;
+                }
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if (marks[i] < 1 || marks[i] > 6)
+                    {
+                        return;
+                    }
+                }
+                for (int i = 0; i < 7; i++)
+                {
+                    _marks[_jumpIndex, i] = marks[i];
+                }
+                _jumpIndex++;
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null || array.Length < 2) return;
+
+                int n = array.Length;
+                for (int i = 1, j = 2; i < n;)
+                {
+                    if (i == 0 || array[i].TotalScore < array[i - 1].TotalScore)
+                    {
+                        i = j;
+                        j++;
+                    }
+                    else
+                    {
+                        Participant temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                        i--;
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                if (_surname == "" || _name == "")
+                {
+                    return;
+                }
+
+                Console.WriteLine($"{_name} {_surname} {TotalScore:F2}");
+            }
+        }
+
+        public class Judge
+        {
+            private string _name;
+            private int[] _marks;
+            private int _marksOffset;
+
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+            }
+
+            public Judge(string name, int[] marks)
+            {
+                _name = name;
+                if (marks is null)
+                {
+                    _marks = null;
+                    return;
+                }
+                _marksOffset = 0;
+
+                _marks = new int[marks.Length];
+
+                Array.Copy(marks, _marks, marks.Length);
+            }
+
+            public int CreateMark()
+            {
+                if (_marks == null || _marks.Length == 0)
+                    return 0;
+                if (_marksOffset > _marks.Length)
+                    _marksOffset %= _marks.Length;
+                return _marks[_marksOffset++];
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{_name}");
+
+                PrintArr(_marks);
+            }
+        }
+
+        public class Competition
+        {
+            private Judge[] _judges;
+            private Participant[] _participants;
+
+            public Competition(Judge[] judges)
+            {
+
+                if (judges is null || judges.Length != 7)
+                {
+                    return;
+                }
+
+                _judges = new Judge[judges.Length];
+                _participants = new Participant[0];
+
+                Array.Copy(judges, _judges, judges.Length);
+            }
+
+            public void Evaluate(Participant jumper)
+            {
+                if (jumper is null || _judges is null)
+                    return;
+
+                int[] marks = new int[_judges.Length];
+
+                for (int j = 0; j < _judges.Length; j++)
+                {
+                    marks[j] = _judges[j].CreateMark();
+                    if (marks[j] == 0)
+                        return;
+                }
+
+                jumper.Jump(marks);
+            }
+
+            public void Add(Participant participant)
+            {
+                if (participant is null)
+                    return;
+
+                if (_participants is null)
+                    _participants = new Participant[0];
+
+                Array.Resize(ref _participants, _participants.Length + 1);
+
+                _participants[_participants.Length - 1] = participant;   
+
+            }
+
+            public void Add(Participant[] participants)
+            {
+                if (participants is null || participants.Length == 0)
+                    return;
+
+                int cnt = 0;
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i] != null)
+                        cnt++;
+                }
+
+                Array.Resize(ref _participants, _participants.Length + cnt);
+
+                for (int i = 0, k = _participants.Length - cnt; i < participants.Length; i++)
+                {
+                    if (participants[i] != null)
+                        _participants[k++] = participants[i];
+                }
+
+            }
+
+            public void Sort()
+            {
+                Participant.Sort(_participants);
+            }
+
+            public void Print()
+            {
+                if (_judges != null)
+                {
+                    for (int i = 0; i <  _judges.Length; i++)
+                    {
+                        if (_judges[i] != null)
+                            _judges[i].Print(); 
+                    }
+                }
+
+                if (_participants != null)
+                {
+                    for (int i = 0; i < _participants.Length; i++)
+                    {
+                        if (_participants[i] != null)
+                            _participants[i].Print();
+                    }
+                }
+            }
+        }
+
+       
+
+        public static void PrintArr(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return;
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+            Console.WriteLine();
+        }
+
+        public static void PrintArr(double[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return;
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+            Console.WriteLine();
+        }
+
+        public static void PrintArr(Participant[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return;
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i].TotalScore} ");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Lab-7/Purple_2.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    public class Purple_2
+    {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private int _distance;
+            private int[] _marks;
+            private int _result;
+
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+            }
+
+            public string Surname
+            {
+                get
+                {
+                    return _surname;
+                }
+            }
+
+            public int Distance
+            {
+                get
+                {
+                    return _distance;
+                }
+            }
+
+            public int[] Marks
+            {
+                get
+                {
+                    if (_marks is null) return null;
+                    int[] marksCopy = new int[_marks.Length];
+                    Array.Copy(_marks, marksCopy, marksCopy.Length);
+                    return marksCopy;
+                }
+            }
+
+            public int Result
+            {
+                get
+                {
+
+                    return _result;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _distance = 0;
+                _marks = new int[5];
+                _result = 0;
+            }
+
+            public void Jump(int distance, int[] marks, int target)
+            {
+                if (_marks is null || marks is null || marks.Length != 5) return;
+
+                if (target < 0) target = 0;
+
+                if (distance < 0) return;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (marks[i] < 1 || marks[i] > 20)
+                    {
+                        return;
+                    }
+                }
+
+                _distance = distance;
+                int maxMarkIdx = 0, minMarkIdx = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    _marks[i] = marks[i];
+                    if(_marks[i] > _marks[maxMarkIdx])
+                        maxMarkIdx = i;
+                    if (_marks[i] < _marks[minMarkIdx])
+                        minMarkIdx = i;
+                    _result += _marks[i];
+                }
+
+                _result -= _marks[maxMarkIdx];
+                _result -= _marks[minMarkIdx];
+
+                _result += 60 + (_distance - target) * 2;
+
+                if (_result < 0) _result = 0;
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null || array.Length < 2) return;
+
+                int n = array.Length;
+                for (int i = 1, j = 2; i < n;)
+                {
+                    if (i == 0 || array[i].Result < array[i - 1].Result)
+                    {
+                        i = j;
+                        j++;
+                    }
+                    else
+                    {
+                        Participant temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                        i--;
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                if (_surname == "" || _name == "")
+                {
+                    return;
+                }
+
+                Console.WriteLine($"{_name} {_surname} {Result}");
+            }
+        }
+
+        public abstract class SkiJumping
+        {
+            private string _name;
+            private int _standard;
+            private Participant[] _participants;
+            private int _offsetJump;
+
+
+            public string Name
+            {
+                get { return _name; }
+            }
+
+            public int Standard
+            {
+                get { return _standard; }
+            }
+
+            public Participant[] Participants
+            {
+                get {
+                    if (_participants == null)
+                        return null;
+
+                    Participant[] participantsCopy = new Participant[_participants.Length];
+                    Array.Copy(_participants, participantsCopy, _participants.Length);
+
+                    return participantsCopy;
+                }
+            }
+
+            public SkiJumping(string name, int standart) { 
+                _name = name;
+                _standard = standart;
+                _participants = new Participant[0];
+                _offsetJump = 0;
+            }
+
+            public void Add(Participant participant)
+            {
+
+                if (_participants is null)
+                    _participants = new Participant[0];
+
+                Array.Resize(ref _participants, _participants.Length + 1);
+
+                _participants[_participants.Length - 1] = participant;
+
+            }
+
+            public void Add(Participant[] participants)
+            {
+                if (participants is null || participants.Length == 0)
+                    return;
+
+
+                Array.Resize(ref _participants, _participants.Length + participants.Length);
+
+                for (int i = 0, k = _participants.Length - participants.Length; i < participants.Length; i++)
+                {
+                    _participants[k++] = participants[i];
+                }
+
+            }
+
+            public void Jump(int distance, int[] marks)
+            {
+                if (_participants == null || _participants.Length == 0 || _offsetJump == _participants.Length) return;
+
+                _participants[_offsetJump++].Jump(distance, marks, _standard);
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_name);
+                Console.WriteLine(_standard);
+                if (_participants is null)
+                    return;
+
+                for (int i = 0; i < _participants.Length; i++)
+                    _participants[i].Print();
+            }
+        }
+
+        public class JuniorSkiJumping: SkiJumping
+        {
+
+            public JuniorSkiJumping(): base("100m", 100){}
+        }
+
+        public class ProSkiJumping : SkiJumping
+        {
+
+            public ProSkiJumping() : base("150m", 150) { }
+        }
+    }
+}
